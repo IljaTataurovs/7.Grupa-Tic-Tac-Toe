f32a7168feda373e21e9db8f67d2b0452ff44346
ilja Tataurov <ilja.tataurov@gmail.com>
2022-05-10 22:07:10 +0300

    Lekcijas laika

---
+++ TicTacToe/TicTacToe/Program.cs
@@ -0,0 +1,16 @@
+﻿
+Console.WriteLine("Hello, do you want to play a game?");
+Console.WriteLine();
+
+int evenNumber = 0;
+
+
+char[] array = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+
+Console.WriteLine("[        |        |        ]");
+Console.WriteLine($"          {0}  |     {1}   |   {2}    ",array[1],array[2],array[3]);
+Console.WriteLine("[        |        |        ]");
+Console.WriteLine("[        |        |        ]");
+//Console.WriteLine($" [ {array1} ]");
+//Console.WriteLine($" [ {array2} ]");
+//Console.WriteLine(array3);
