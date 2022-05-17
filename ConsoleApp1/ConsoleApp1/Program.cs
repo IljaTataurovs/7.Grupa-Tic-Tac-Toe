c5c8d99cdf2b5345721dab00e46962adc3e83383
DainisMors <mors.dainis@gmail.com>
2022-05-17 21:24:28 +0300

    gg

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -9,7 +9,7 @@ void brainOfGame(char player)
 {
     while (true)
     {
-        Console.Clear();
+        Console.Clear(); //koment1
         printBoard(board);
         Console.WriteLine($"Player {player} make your move! Please choose square were to put {player} sign (2 cipari kā koordināte, piemēram 11 vai 23, vai ...)");
         string info = Console.ReadLine();
