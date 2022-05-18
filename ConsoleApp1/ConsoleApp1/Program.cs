4824e8261073073845303878222d1444483a9073
ilja Tataurov <ilja.tataurov@gmail.com>
2022-05-18 16:30:26 +0300

    new changes

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,6 +1,6 @@
 ﻿Console.Title = "Tic Tac Toe";
 char player = 'X';
-char[,] board = new char[4, 4];
+char[,] board = new char[3, 3];
 int movesPlayed = 0;
 menu();
 
@@ -9,36 +9,140 @@ void brainOfGame(char player)
 {
     while (true)
     {
-        Console.Clear(); //koment1ggwwwcc
+        Console.Clear(); 
         printBoard(board);
-        Console.WriteLine($"Player {player} make your move! Please choose square were to put {player} sign (2 cipari kā koordināte, piemēram 11 vai 23, vai ...)");
+        Console.WriteLine($"Player {player} make your move! Please choose square were to put {player} sign ");
+        Console.WriteLine($"2 cipari kā koordināte, piemēram 11 vai 23, vai ...");
+        Console.WriteLine($"Pirmais skaitlis ir row(sarkans) Otrais skaitlis ir column (zals)");
         string info = Console.ReadLine();
+        if (!info.Equals(11) || !info.Equals(12) || !info.Equals(13) || !info.Equals(21) || !info.Equals(22) || !info.Equals(23) || !info.Equals(31) || !info.Equals(32) || !info.Equals(33))
+        {
+            Console.WriteLine("Incorrect input1");
+        }
 
         int row = int.Parse(info.Substring(0,1));
         int column = int.Parse(info.Substring(1,1));
-        
-
-
-        board[row, column] = player;
-
-        if (player == board[1, 1] && player == board[1, 2] && player == board[1, 3])//parbaudam var ir vinnejs
+        if (row < 1 || column < 1|| row > 3 || column >3)
         {
-            Console.WriteLine($"Player {player} has won the game!");
-            break;
+            Console.WriteLine("Incorrect input2");
         }
+
+       //board[row-1, column-1] = player;//user ievadija 11 =(0,0) 22=(1,1)
+       
+
+        Move(row, column, player);
+
+        //if (player == board[0, 0] && player == board[0, 1] && player == board[0, 2])//parbaudam var ir uzvaretajs pirma rinda
+        //{
+        //    Console.Clear();
+        //    printBoard(board);
+        //    Console.WriteLine($"Player {player} has won the game!");
+        //    break;
+        //}
+        //if (player == board[3, 1] && player == board[2, 2] && player == board[1, 3])//parbaudam var ir uzvaretajs diagonala rinda
+        //{
+        //    Console.Clear();
+        //    printBoard(board);
+        //    Console.WriteLine($"Player {player} has won the game!");
+        //    break;
+        //}
+        //if (player == board[0, 0] && player == board[1, 1] && player == board[2, 2])//parbaudam var ir uzvaretajs diagonala rinda 2
+        //{
+        //    Console.Clear();
+        //    printBoard(board);
+        //    Console.WriteLine($"Player {player} has won the game!");
+        //    break;
+        //}
+        //if (player == board[1, 0] && player == board[1, 1] && player == board[1, 2])//parbaudam var ir uzvaretajs otra rinda
+        //{
+        //    Console.Clear();
+        //    printBoard(board);
+        //    Console.WriteLine($"Player {player} has won the game!");
+        //    break;
+        //}
+        //if (player == board[2, 0] && player == board[2, 1] && player == board[2, 2])//parbaudam var ir uzvaretajs tresaja rinda
+        //{
+        //    Console.Clear();
+        //    printBoard(board);
+        //    Console.WriteLine($"Player {player} has won the game!");
+        //    break;
+        //}
+        //if (player == board[0, 0] && player == board[1, 0] && player == board[2, 0])//parbaudam var ir uzvaretajs 1 kolonna
+        //{
+        //    Console.Clear();
+        //    printBoard(board);
+        //    Console.WriteLine($"Player {player} has won the game!");
+        //    break;
+        //}
+        //if (player == board[0, 1] && player == board[1, 1] && player == board[2, 1])//parbaudam var ir uzvaretajs 2 kolonna
+        //{
+        //    Console.Clear();
+        //    printBoard(board);
+        //    Console.WriteLine($"Player {player} has won the game!");
+        //    break;
+        //}
+        //if (player == board[0, 2] && player == board[1, 2] && player == board[2, 2])//parbaudam var ir uzvaretajs 3 kolonna
+        //{
+        //    Console.Clear();
+        //    printBoard(board);
+        //    Console.WriteLine($"Player {player} has won the game!");
+        //    break;
+        //}
+        //else
+        //{
+        //    Console.WriteLine($"Incorrect input3");
+        //}
 
         movesPlayed++;//skaitam kartas cik nospeletas ja 9 tad ir neizskirts
         if (movesPlayed == 9)
         {
-            Console.WriteLine("No one won a game! it's a draw!");//ppffee
+            Console.Clear();
+            printBoard(board);
+            Console.WriteLine("No one won a game! it's a draw!");
             break;
         }
+      
         player = changePlayer(player);
 
     }
 }
 
+void Move(int row, int column,char player)
+{
+    if (board[row, column] == ' ')
+    {
+        board[row, column] = player;
+    }
 
+    for (int i = 0; i < 3; i++)//check rows
+    {
+        if (board[i, column] != player)
+        {
+            break;
+        }
+        if (i == 2)
+        {
+            Console.Clear();
+            printBoard(board);
+            Console.WriteLine($"Player {player} has won the game!");
+            break;
+        }
+    }
+    for (int i = 0; i < 3; i++)//check columns
+    {
+        if (board[row, i] != player)
+        {
+            break;
+        }
+        if (i == 2)
+        {
+            Console.Clear();
+            printBoard(board);
+            Console.WriteLine($"Player {player} has won the game!");
+            break;
+        }
+    }
+}
 
 static char changePlayer(char currentplayer)//mainam speletaju
 {
@@ -54,9 +158,9 @@ static char changePlayer(char currentplayer)//mainam speletaju
 
 void printStartBoard(char[,] board)//inicializejam laukumu
 {
-    for (int row = 1; row < 4; row++)
+    for (int row = 0; row < 3; row++)
     {
-        for (int col = 1; col < 4; col++)
+        for (int col = 0; col < 3; col++)
         {
             board[row, col] = ' ';
 
@@ -68,18 +172,25 @@ void printStartBoard(char[,] board)//inicializejam laukumu
 
 void printBoard(char[,] board)//izprintejam laukumu
 {
-    Console.WriteLine($"__|_1_|_2_|_3_|");
-    for (int row = 1; row < 4; row++)
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine($"    0   1   2  ");
+    Console.ForegroundColor = ConsoleColor.White;
+    Console.WriteLine($"__|___|___|___|");
+   
+    for (int row = 0; row < 3; row++)
     {
-        Console.Write($"{row} | ");
-        for (int col = 1; col < 4; col++)
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.Write($"{row }");
+        Console.ForegroundColor = ConsoleColor.White;
+        Console.Write($" | ");
+        for (int col = 0; col < 3; col++)
         {
             Console.Write(board[row, col]);
             Console.Write(" | ");
         }
         Console.WriteLine();
     }
-
+    Console.WriteLine();
 }
 
 void menu()//menu kur lietotajs var izveleties vai vins grib uzspelet speli
