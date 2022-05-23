a2f59929d096413a102d4c8288572a3ea10f491b
iljat <ilja.tataurov@gmail.com>
2022-05-23 12:19:37 +0300

    paliek sakartot failus

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,4 +1,5 @@
-﻿Console.Title = "Tic Tac Toe";
+﻿
+Console.Title = "Tic Tac Toe";
 char player = 'X';
 int n = 3;
 char[,] board = new char[n, n];
@@ -18,105 +19,108 @@ void StartGame(char player)
         Console.WriteLine($"2 cipari kā koordināte, piemēram 11 vai 23, vai ...");
         Console.WriteLine($"Pirmais skaitlis ir row(sarkans) Otrais skaitlis ir column (zals)");
         string info = Console.ReadLine();
-        if (!info.Equals(11) || !info.Equals(12) || !info.Equals(10) || !info.Equals(21) || !info.Equals(22) || !info.Equals(20) || !info.Equals(01) || !info.Equals(02) || !info.Equals(00))
-        {
-            Console.WriteLine("Incorrect input1");
-        }
 
-        int row = int.Parse(info.Substring(0,1));
-        int column = int.Parse(info.Substring(1,1));
-        if (row < 0 || column < 0|| row > n || column > n)
-        {
-            Console.WriteLine("Incorrect input2");
-        }
-     
-        if (board[row, column] == ' ')//ja laukums ir tukss tad mes pieskiram X vai 0(player)
-        {
+        int row = int.Parse(info.Substring(0, 1));
+        int column = int.Parse(info.Substring(1, 1));
 
-            board[row, column] = player;
-
-            movesPlayed++;//skaitam kartas cik nospeletas ja 9 tad ir neizskirts
-
-        }
-        else
-        {
-            // ... output an error message
-            Console.WriteLine("That spot is already taken!...press any key");
-            movesPlayed--;
-            Console.ReadKey();
-            player = ChangePlayer(player);
-
-
-        }
-
-        for (int i = 0; i < n; i++)//check rows
-        {
-            if (board[i, column] != player)
-            {
-                break;
-            }
-            if (i == n - 1)
-            {
-                OutputWinner();
- 
-            }
-        }
-        for (int i = 0; i < n; i++)//check columns
-        {
-            if (board[row, i] != player)
-            {
-                break;
-            }
-            if (i == n - 1)
-            {
-                OutputWinner();
-               
-            }
-        }
-        if (row == column) //check diagonal
-        {
-            for (int i = 0; i < n; i++)
-            {
-                if (board[i, i] != player)
-                {
-                    break;
-                }
-                if (i == n - 1)
-                {
-                    OutputWinner();
-        
-                }
-            }
-        }
-        if ((row + column) == n - 1) //check diagonal diffrent direction
-        {
-            for (int i = 0; i < n; i++)
-            {
-                if (board[i, (n - 1) - i] != player)
-                {
-                    break;
-                }
-                if (i == n - 1)
-                {
-                    OutputWinner();
       
+
+            if (row < 0 || column < 0 || row > n || column > n)
+            {
+            Console.WriteLine("Input error");
+            }
+          
+
+            if (board[row, column] == ' ')//ja laukums ir tukss tad mes pieskiram X vai 0(player)
+            {
+
+                board[row, column] = player;
+
+                movesPlayed++;//skaitam kartas cik nospeletas ja 9 tad ir neizskirts
+
+            }
+            else
+            {
+                // ... output an error message
+                Console.WriteLine("That spot is already taken!...press any key");
+                movesPlayed--;
+                Console.ReadKey();
+                player = ChangePlayer(player);
+
+
+            }
+
+
+            for (int i = 0; i < n; i++)//check rows
+            {
+                if (board[i, column] != player)
+                {
+                    break;
+                }
+                if (i == n - 1)
+                {
+                    OutputWinner();
+
                 }
             }
-        }
+            for (int i = 0; i < n; i++)//check columns
+            {
+                if (board[row, i] != player)
+                {
+                    break;
+                }
+                if (i == n - 1)
+                {
+                    OutputWinner();
+
+                }
+            }
+            if (row == column) //check diagonal
+            {
+                for (int i = 0; i < n; i++)
+                {
+                    if (board[i, i] != player)
+                    {
+                        break;
+                    }
+                    if (i == n - 1)
+                    {
+                        OutputWinner();
+
+                    }
+                }
+            }
+            if ((row + column) == n - 1) //check diagonal diffrent direction
+            {
+                for (int i = 0; i < n; i++)
+                {
+                    if (board[i, (n - 1) - i] != player)
+                    {
+                        break;
+                    }
+                    if (i == n - 1)
+                    {
+                        OutputWinner();
+
+                    }
+                }
+            }
 
 
-        if (movesPlayed == 9)
-        {
-            Console.Clear();
-            PrintBoard(board);
-            Console.WriteLine("No one won a game! it's a draw!");
-            Console.WriteLine("Please press any key for main menu");
-            Console.ReadKey();
-            Menu();
-            
-        }
 
-        player = ChangePlayer(player);
+            if (movesPlayed == 9)
+            {
+                Console.Clear();
+                PrintBoard(board);
+                Console.WriteLine("No one won a game! it's a draw!");
+                Console.WriteLine("Please press any key for main menu");
+                Console.ReadKey();
+                Menu();
+
+            }
+
+            player = ChangePlayer(player);
+        
     }
 }
 
@@ -143,7 +147,7 @@ void OutputWinner()//izprintejam uzvaretaju
     isValid = false;
     movesPlayed = 0;
     Console.ReadKey();
-    Menu();
+    Menu2();
 
 }
 
@@ -189,7 +193,7 @@ void Menu()//menu kur lietotajs var izveleties vai vins grib uzspelet speli
 {
 
     Console.Clear();
-    Console.WriteLine("=====Main Menu========================");
+    Console.WriteLine("==============Main Menu===============");
     Console.WriteLine("Hello, do you want to play a game?");
     Console.WriteLine("Please make a choise! ");
     Console.WriteLine("======================================");
@@ -205,6 +209,35 @@ void Menu()//menu kur lietotajs var izveleties vai vins grib uzspelet speli
             PrintStartBoard(board);
             StartGame(player);
             break;
+        case 2:
+            Console.WriteLine("See you next time!");
+            break;
+        default:
+            Console.WriteLine("Incorrect option...press any key");
+            Console.ReadKey();
+            break;
+    }
+}
+void Menu2()//menu2 pec vienas speles
+{
+
+    Console.Clear();
+    Console.WriteLine("==============Main Menu===============");
+    Console.WriteLine("Hello, do you want to play another game?");
+    Console.WriteLine("Please make a choise! ");
+    Console.WriteLine("======================================");
+    Console.WriteLine("1: Yes! Let's play a game! ");
+    Console.WriteLine("2: No! Leave me alone! ");
+    Console.WriteLine("======================================");
+    isValid = true;
+    int userChoise2 = int.Parse(Console.ReadLine());
+    switch (userChoise2)
+    {
+        case 1:
+            Console.Clear();
+            PrintStartBoard(board);
+            StartGame(player);
+           break;
         case 2:
             Console.WriteLine("See you next time!");
             break;
