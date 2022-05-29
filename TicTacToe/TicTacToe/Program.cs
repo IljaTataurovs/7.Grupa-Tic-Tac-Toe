5488bf0cc2d2472f192bad045d1d2bca0d6ec455
DainisMors <mors.dainis@gmail.com>
2022-05-29 12:26:52 +0300

    dm2

--- TicTacToe/TicTacToe/Program.cs
+++ TicTacToe/TicTacToe/Program.cs
@@ -6,38 +6,35 @@ Console.Title = "Tic Tac Toe";
 
 Field field = new Field();
 Match match = new Match();
-Menu();
-void Menu()
-{
 
-    Console.Clear();
-    Console.WriteLine("==============Main Menu===============");
-    Console.WriteLine("Hello, do you want to play a game?");
-    Console.WriteLine("Please make a choice! ");
-    Console.WriteLine("======================================");
-    Console.WriteLine("1: Yes! Let's play a game! ");
-    Console.WriteLine("2: No! Leave me alone! ");
-    Console.WriteLine("======================================");
+Console.Clear();
+Console.WriteLine("==============Main Menu===============");
+Console.WriteLine("Hello, do you want to play a game?");
+Console.WriteLine("Please make a choice! ");
+Console.WriteLine("======================================");
+Console.WriteLine("1: Yes! Let's play a game! ");
+Console.WriteLine("2: No! Leave me alone! ");
+Console.WriteLine("======================================");
 
-    int userChoice = int.Parse(Console.ReadLine());
-    switch (userChoice)
-    {
-        case 1:
-            Console.Clear();
-            Field.PrintStartBoard(match.board);
-            //Match.StartGame();
-            Field.PrintBoard(match.board);
-            // Console.WriteLine($"Waiting for player{match.player}");
-            Position position = field.readPosition();
-            // match.makeMove(position)
-            //StartGame(player);
-            break;
-        case 2:
-            Console.WriteLine("See you next time!");
-            break;
-        default:
-            Console.WriteLine("Incorrect option...press any key");
-            Console.ReadKey();
-            break;
+int userChoice = int.Parse(Console.ReadLine());
+
+switch (userChoice)
+   {
+     case 1:
+        Console.Clear();
+        Field.PrintStartBoard(match.board);
+        //Match.StartGame();
+        Field.PrintBoard(match.board);
+        // Console.WriteLine($"Waiting for player{match.player}");
+        Position position = field.readPosition();
+        // match.makeMove(position)
+        //StartGame(player);
+        break;
+    case 2:
+        Console.WriteLine("See you next time!");
+        break;
+    default:
+        Console.WriteLine("Incorrect option...press any key");
+        Console.ReadKey();
+        break;
     }
-}
